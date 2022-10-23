e74f9c82b247494876cf1d1f6d66101fa33a9c8a
Juliya Khanenko <jullietnm@gmail.com>
2022-10-23 23:51:47 +0300

    vision

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Select from 'react-select';
 import { GlobalStyle } from './GlobalStyle';
 
-axios.defaults.baseURL = 'https://thedogapi.com/v1/';
+axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
 axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
 
 export class App extends Component {
@@ -15,7 +15,7 @@ export class App extends Component {
   async componentDidMount() {
     try {
       const response = await axios.get('/breeds');
-      this.setState({ bpeeds: response.data });
+      this.setState({ breeds: response.data });
     } catch (error) {}
   }
 
